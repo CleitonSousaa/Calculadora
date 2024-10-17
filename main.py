e5d54449388997d054492321d46515d18fce0905
cleiton sousaa <fccleitonfs.329@gmail.com>
2024-10-17 16:05:47 -0300

    update

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
-import kivy
-from kivy.app import App
-from kivy.uix.label import Label
+import tkinter as tk
 
-class MeuApp(App):
-    def build(self):
-        return Label(text="Testando")
+def calculate(operation):
+    global expression 
 
-if __name__ == "__main__":
-    MeuApp().run()
+    if operation == 'C':
+        expression = ""
+    elif operation =='<':
+        expression = expression[:-1]
+    else:
+        if expression == "" and operation in ['+', '-', '*', '/']:
+            return
+        expression += operation
+    
+    label_text.set(expression)
+
+def evaluate():
+    global expression
+    try:
+        result = str(eval(expression))
+        label_text.set(result)
+        expression = result
+    except:
+        label_text.set("Error")
+        expression = ""
+
+root = tk.Tk()
+root.title("Calculadora simples")
+
+
+expression = ""
+label_text = tk.StringVar()
+
+label = tk.Label(root, textvariable=label_text, font=('Arial', 30), bg='white', width=22, height=2)
+label.grid(row=0, column=0, columnspan=4)
+
+
+buttons = [
+    ('7', 1, 0),('8', 1, 1),('9', 1, 2),('C', 1, 3),
+    ('4', 2, 0),('5', 2, 1),('6', 2, 2),('+', 2, 3),
+    ('1', 3, 0),('2', 3, 1),('3', 3, 2),('-', 3, 3),
+    ('.', 4, 0),('0', 4, 1),('<', 4, 2),('*', 4, 3),
+    ('/', 5, 0),('=', 5, 1),
+]
+
+for text, row, col in buttons:
+    action = lambda x=text: calculate(x) if x != '=' else evaluate
+    button = tk.Button(root, text=text, command=action, font=('Arial', 30), width=5, height=2)
+    button.grid(row=row, column=col)
+
+button = tk.Button(root, text='=', command=evaluate, font=('Arial', 30), width=16, height=2)
+button.grid(row=5, column=1, columnspan=3)
+
+root.mainloop()
